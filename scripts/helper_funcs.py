e27ec609682bef5097bc16bcf719d2ff4e4b2d76
srikanthmalla <mallasrikanth004@gmail.com>
2017-08-07 18:49:21 -0400

    curvature added

--- scripts/helper_funcs.py
+++ scripts/helper_funcs.py
@@ -77,6 +77,7 @@ def detect_lanes(warped):
 	if len(window_centroids) > 0:
 		# print(window_centroids)
 		window_centroids=np.asarray(window_centroids)
+
 		leftx=window_centroids[:,0]
 		rightx=window_centroids[:,1]
 		lefty=range(warped.shape[0],0,-window_height)
@@ -94,6 +95,20 @@ def detect_lanes(warped):
 		
 		warped=np.expand_dims(warped,axis=2)
 		warped=np.tile(warped,(1,1,3))
+
+		# Define conversions in x and y from pixels space to meters
+		y_eval = np.max(ploty)
+		ym_per_pix = 30/720 # meters per pixel in y dimension
+		xm_per_pix = 3.7/700 # meters per pixel in x dimension
+
+		# Fit new polynomials to x,y in world space
+		left_fit_cr = np.polyfit(ploty*ym_per_pix, left_fitx*xm_per_pix, 2)
+		right_fit_cr = np.polyfit(ploty*ym_per_pix, right_fitx*xm_per_pix, 2)
+		# Calculate the new radii of curvature
+		left_curverad = ((1 + (2*left_fit_cr[0]*y_eval*ym_per_pix + left_fit_cr[1])**2)**1.5) / np.absolute(2*left_fit_cr[0])
+		right_curverad = ((1 + (2*right_fit_cr[0]*y_eval*ym_per_pix + right_fit_cr[1])**2)**1.5) / np.absolute(2*right_fit_cr[0])
+		# Now our radius of curvature is in meters
+		print(left_curverad, 'm', right_curverad, 'm')
 
 		# plt.imshow(warped)
 		# plt.plot(left_fitx, ploty, color='yellow')
